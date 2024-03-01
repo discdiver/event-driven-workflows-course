e218c73e3e772f3e417658abc5ec7f5c66e204f2
Jeff Hale <jeffmshale@gmail.com>
2024-03-01 12:40:32 -0500

    add basic event

--- module_2/project_solution_create_deploy_recommend.py
+++ module_2/project_solution_create_deploy_recommend.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     flow.from_source(
         source="https://github.com/discdiver/event-driven-workflows-course.git",
         entrypoint="module_2/project_solution_recommend.py:recommend_shares",
-    ).deploy(name="recommend-deployment", work_pool_name="managed2", interval=10)
+    ).deploy(name="recommend-deployment", work_pool_name="managed2", interval=[100, 30])
